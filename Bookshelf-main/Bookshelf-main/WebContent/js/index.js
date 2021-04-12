e046b1b4a94cdb0a33edfa0f394cb9dd46238848
maeldetraverse <49312349+maeldetraverse@users.noreply.github.com>
2021-04-12 10:58:31 +0200

    Add files via upload

---
+++ Bookshelf-main/Bookshelf-main/WebContent/js/index.js
@@ -0,0 +1,61 @@
+var bookObject = [
+    {
+        "title": "Alexandre Dumas ",
+        "id": "ec1040",
+        "color": "White",
+        "price": 9.99,
+        "availability": "Available"
+    },
+    {
+        "title": "C.S. Lewis",
+        "id": "ec1041",
+        "color": "White",
+        "price": 8.50,
+        "availability": "Out of stock"
+    },
+    {
+        "title": "Charles Dickens",
+        "id": "ec1042",
+        "color": "Brown",
+        "price": 11.60,
+        "availability": "Available"
+    },
+    {
+        "title": "Jane Austen",
+        "id": "ec1043",
+        "color": "Rose Gold",
+        "price": 7.50,
+        "availability": "Available"
+    },
+    {
+        "title": "Jonathan Swift",
+        "id": "ec1043",
+        "color": "Tan Brown",
+        "price": 4.99,
+        "availability": "Out of stock"
+    }
+]
+var temp = [];
+function addtoCart(bookName) {
+    temp.push(bookName);
+    sessionStorage.setItem('bookCart', JSON.stringify(temp));
+    $("#basket").css("display", "block");
+}
+
+// function viewBasket() {
+//     $("#basket").css("display", "block");
+// }
+
+// Generic close popup button (pass ID as a parameter to this function)
+function closePopup(popupID) {
+    $("#" + popupID).css("display", "none");
+}
+
+// NEWSLETTER Popup code START
+setTimeout(function () {
+    openNewsletterPopup();
+}, 2000);
+function openNewsletterPopup() {
+    $("#newsletter").css("display", "block");
+}
+// NEWSLETTER Popup code END
