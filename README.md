4ff36d319d6c0cdf60e91d0a00c6bb46d9629b7c
conlonj25 <conlonj25@users.noreply.github.com>
2021-04-12 17:41:41 +0100

    Added SQL scripts

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EE_417_WebProject
 
-/Bookshelf contains the Eclipse project. Just clone a copy of the repo to your own machine. In Eclipse click File > Open Projects From File System and select the Bookshelf directory.
+Bookshelf contains the Eclipse project. Just clone a copy of the repo to your own machine. In Eclipse click File > Open Projects From File System and select the Bookshelf directory.
+
+SQL Scripts contains SQL scripts. They're just text files with an .sql extension.
---
+++ SQL Scripts/fill user.sql
@@ -0,0 +1,8 @@
+INSERT INTO testdb.bookshelf_user
+(username, password, first_name, last_name, email, address, zipcode)
+VALUES
+('jamesc10', 'password', 'James', 'Conlon', 'james@mail.com', 'Dublin', 'DRB 4G32'),
+('keatonc16', 'pass1', 'Michael', 'Keaton', 'mkeaton@gmail.com', 'Hollywood', 'BAT 3249'),
+('tjones', 'mypass', 'Tom', 'Jones', 'tom@tomjones.com', 'Wales', '302 AM12'),
+('markymark', 'password1', 'Mark', 'Wahlberg', 'mark@wahlberg.com', 'Bawston', 'H56 GD49'),
+('marymac', 'abcdefg', 'Mary', 'McAleese', 'mary@gov.ie', 'Belfast', '901 QBR6')
